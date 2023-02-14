635e9b776b335aa7cffb0c7458bff41cbd1cdff2
SandraVLP <aleksanvp@yandex.ru>
2023-02-14 23:51:50 +0300

    unerr

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,7 +3,6 @@ const jwt = require('jsonwebtoken'); // импортируем модуль json
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 const User = require('../models/user');
-const UnauthorizedError = require('../errors/unauthorized-err');
 const BadRequestError = require('../errors/bad-request-err');
 const NotFoundError = require('../errors/not-found-err');
 const HTPPConflictError = require('../errors/http-conflict-err');
@@ -111,8 +110,5 @@ module.exports.login = (req, res, next) => {
       // вернём токен
       res.status(200).send({ token });
     })
-    .catch((err) => {
-      console.log('err', err);
-      next(new UnauthorizedError('Пользователь не найден.'));
-    });
+    .catch(next);
 };
--- backend/models/user.js
+++ backend/models/user.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 
 const isEmail = require('validator/lib/isEmail');
+const UnauthorizedError = require('../errors/unauthorized-err');
 
 // eslint-disable-next-line no-useless-escape
 const urlRegExp = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/;
@@ -51,13 +52,13 @@ userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new Error('Неправильные почта или пароль'));
+        return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
       }
 
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new Error('Неправильные почта или пароль'));
+            return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
           }
 
           return user; // теперь user доступен
